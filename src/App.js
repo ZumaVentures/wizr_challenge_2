7921ecade9db7b1a277d08e313121aff860f5147
Jacob Beltran <hello@jacobbeltran.com>
2018-02-25 19:06:09 -0800

    Add useful comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/**
+ * <App />
+ * This component is rendered by the top level <Route />
+ * component to be used as a container for other rendered
+ * components
+*/
 class App extends Component {
   render() {
     return (
--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,7 @@
+/**
+ * Import routes from other sections and combine them here.
+ * The export of this file is used to define child <Route />
+ * matches for the top-level <Router /> component
+*/
 const routes = null;
 export default routes;
